90dc3b522704ca8426e2675a67124247839e159e
Louise Damm Angelius <131687178+LouiseAngelius@users.noreply.github.com>
2023-11-11 14:53:07 +0100

    "html"

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // Import necessary modules
 const express = require('express');
+const path = require('path');
 
 // Create an Express application
 const app = express();
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Set the port for the server to listen on
@@ -14,5 +18,5 @@ const port = 2000;
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
