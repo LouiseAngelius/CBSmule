5a995f125bd4905a0a0041a9927214afd739f9ed
Louise Damm Angelius <131687178+LouiseAngelius@users.noreply.github.com>
2023-11-11 17:22:14 +0100

    "server"

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-// Import necessary modules
 const express = require('express');
 const path = require('path');
-
-// Create an Express application
 const app = express();
+const port = 2000;
 
-// Define a route
 app.get('/', (req, res) => {
   console.log('Request received for /');
   res.send('CBS Mule launching soon!');
 });
 
-// Set the port for the server to listen on
-const port = 2000;
-
-// Start the server
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://0.0.0.0:${port}`);
 });
