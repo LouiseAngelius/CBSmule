45847613ac84b4612ca82adae9453684f2ead73c
Louise Damm Angelius <131687178+LouiseAngelius@users.noreply.github.com>
2023-11-22 11:01:15 +0100

    ny html

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/chat.html', (req, res) => {
-  res.sendFile(__dirname + '/public/chat.html');
-});
-
-app.get('/events.html', (req, res) => {
-  res.sendFile(__dirname + '/public/events.html');
-});
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
