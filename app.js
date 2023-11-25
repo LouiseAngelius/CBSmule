55799110fd720d58ca4b43724e5201bb8e6ba293
Louise Damm Angelius <131687178+LouiseAngelius@users.noreply.github.com>
2023-11-25 13:03:27 +0100

    css+html fix

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/styles.css');
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.get('/events', (req, res) => {
+  res.sendFile(__dirname + '/public/events.html');
+});
+
+app.get('/friends', (req, res) => {
+  res.sendFile(__dirname + '/public/friends.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
+app.get('/news', (req, res) => {
+  res.sendFile(__dirname + '/public/news.html');
+});
+
+app.get('/pictures', (req, res) => {
+  res.sendFile(__dirname + '/public/pictures.html');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
