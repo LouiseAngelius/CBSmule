d173982ba012046d47f4cb72210b1d3ece7aaa93
Louise Damm Angelius <131687178+LouiseAngelius@users.noreply.github.com>
2023-11-28 08:21:19 +0100

    db connection burde spille

--- src/db/executesql.js
+++ src/db/executesql.js
@@ -1,39 +1,43 @@
-const { Connection, Request } = require("tedious");
+let Connection = require('tedious').Connection;
+let Request = require('tedious').Request;
 const config = require("./config.json");
 
-const executeSQL = (query) => {
-  return new Promise((resolve, reject) => {
-    const connection = new Connection(config);
-    const request = new Request(query, (err) => {
-      if (err) {
-        reject(err);
-      }
-    });
+const executeSQL = (sql) => {
+    return new Promise((resolve, reject) => {
+        let connection = new Connection(config)
 
-    connection.on("connect", (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log("Connected to the database");
-        connection.execSql(request);
-      }
-    });
-    connection.connect();
+        connection.on('connect', function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                const request = new Request(sql, function (err) {
+                    if (err) {
+                        reject(err);
+                    }
+                })
+        
+                connection.execSql(request)
 
-    let response = {};
+                let counter = 0;
+                response = {}
 
-    request.on("row", (columns) => {
-      const rowData = {};
-      columns.forEach((column) => {
-        rowData[column.metadata.colName] = column.value;
-      });
-      response[request.rowCount] = rowData;
-    });
+                request.on('row', function (columns) {
+                    response[counter] = {}
+                    columns.forEach(function (column) {
+                        response[counter][column.metadata.colName] = column.value
+                    });
+                    counter += 1
+                });
 
-    request.on("requestCompleted", () => {
-      resolve(response);
+                request.on('requestCompleted', () => {
+                    resolve(response)
+                });
+            }
+        });
+
+        connection.connect()
     });
-  });
-};
+}
 
 module.exports = { executeSQL };
+// Ovenstående er Nicolais kode jvf youtube video "Connect til MSSQL med node.js" - dette gælder også koden i config.json
