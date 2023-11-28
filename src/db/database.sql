37fc87f9287adcf3eb1f564c1bea12edb6f6fb40
Louise Damm Angelius <131687178+LouiseAngelius@users.noreply.github.com>
2023-11-28 08:17:18 +0100

    database done

--- src/db/database.sql
+++ src/db/database.sql
@@ -1,6 +1,22 @@
-CREATE TABLE users (
-    user_id INT PRIMARY KEY IDENTITY(1,1),
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    UserName VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(50),
+    PhoneNumber VARCHAR(15)  -- Assuming it includes the plus sign for country codes
+);
+
+CREATE TABLE Favorites (
+    FavoritesID INT PRIMARY KEY,
+    Juice VARCHAR(50),
+    Coffee VARCHAR(50),
+    Sandwich VARCHAR(50)
+);
+
+CREATE TABLE UserFavorites (
+    UserFavoritesID INT PRIMARY KEY,
+    UserID INT,
+    FavoritesID INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (FavoritesID) REFERENCES Favorites(FavoritesID)
 );
